5968d07020cb6f81a1a23d4d40798a8b2fd5242f
HimanshuMNirmal <nirmalhimanshu1505@gmail.com>
2024-08-22 19:39:59 +0530

    added password change

--- Backend/edunect/account/urls.py
+++ Backend/edunect/account/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('/login',views.login_view),
     path('/logout',views.logout_view),
     path('/signup',views.signup_view),
+    path('/passChange',views.password_change_view)
 ]
